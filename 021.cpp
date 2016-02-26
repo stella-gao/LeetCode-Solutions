d18e5f6cc13b45eb8885f8f5aad11c370ed1aad3
Stella Gao <stella.gao.89@gmail.com>
2016-02-26 08:26:12 -0500

    Update 021.cpp

--- 021.cpp
+++ 021.cpp
@@ -8,37 +8,27 @@
  */
 class Solution {
 public:
-    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
-        if (l1 == NULL)
-            return l2;
-        if (l2 == NULL)
-            return l1;
-
-        ListNode *start, *cur;
-
-        if (l1->val < l2->val) {
-            cur = start = l1;
-            l1 = l1->next;
-        } else {
-            cur = start = l2;
-            l2 = l2->next;
-        }
-        while (l1 != NULL && l2 != NULL) {
-            if (l1->val < l2->val) {
-                cur->next = l1;
-                cur = l1;
-                l1 = l1->next;
-            } else {
-                cur->next = l2;
-                cur = l2;
-                l2 = l2->next;
+   ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+            ListNode head(INT_MIN);
+            ListNode *p = &head;
+            for(; l1 && l2; p = p->next) {
+                if(l1->val < l2->val) {
+                    p->next = l1;
+                    l1 = l1->next;
+                }
+                else {
+                    p->next = l2;
+                    l2 = l2->next;
+                }
             }
+            if(l1) {
+                p->next = l1;
+            }
+            else {
+                p->next = l2;
+            }
+
+            return head.next;
         }
-        if (l1 != NULL)
-            cur->next = l1;
-        else
-            cur->next = l2;
-        return start;
-    }
 };
 
