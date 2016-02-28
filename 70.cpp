2aa177193048c6d46455052a78ccc5f24892dad2
Stella Gao <stella.gao.89@gmail.com>
2016-02-28 00:04:53 -0500

    Create 70.cpp

---
+++ 70.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 0)
+            return 1;
+        if (n <= 3)
+            return n;
+        int pre1 = 1, pre2 = 2, cur;
+        for (int i = 3; i <= n; i++) {
+            cur = pre1 + pre2;
+            pre1 = pre2;
+            pre2 = cur;
+        }
+        return cur;
+    }
+};
