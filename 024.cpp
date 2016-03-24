8bf954663b58962032923a4185a76016639f9ce2
Stella Gao <stella.gao.89@gmail.com>
2016-03-23 23:55:24 -0400

    Create 024.cpp

---
+++ 024.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode *swapPairs(ListNode *head) {
+        ListNode *H = new ListNode(0);
+        H->next = head;
+        
+        ListNode *pre = H, *cur = head;
+        int cnt = 1;
+        while (cur != NULL && cur->next != NULL) {
+            ListNode *tmp = cur->next;
+            pre->next = tmp;
+            cur->next = tmp->next;
+            tmp->next = cur;
+            
+            pre = cur;
+            cur = cur->next;
+        }
+        head = H->next;
+        delete H;
+        return head;
+    }
+};
+
